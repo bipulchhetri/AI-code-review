0ea4975ee1e23dc4e14198c8a7f721abcbf8085a
bipulchhetri <bipulchhetri80@gmail.com>
2025-04-16 23:16:49 +0530

    footer update

--- frontend/src/component/Footer.jsx
+++ frontend/src/component/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="footer">
       <p className="footer-text">
-      Developved by Bipul &copy; {new Date().getFullYear()} AI Code Reviewer. All rights reserved.
+      Developved by Bipul Chhetri &copy; {new Date().getFullYear()} AI Code Reviewer. All rights reserved.
       </p>
     </footer>
   );
